824ae5ee2f5f405a13d9f3fc61c99b480d2088b2
tatkuptcova <tatkuptcova@yandex.ru>
2021-07-23 20:44:12 +0200

    создан Main.js в него перенесен <main>

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './Header';
+import Main from './Main'
 import '../index.css';
 
 
@@ -7,29 +8,8 @@ function App() {
   return (
     <div className="page">
       <Header />
-
-      <main className="container">
-        <div className="content">
-            <section className="profile">
-              <div className="profile__avatar">
-                <img 
-                className="profile__image" 
-                src="<%=require('./images/avatar.jpg')%>" 
-                alt="Профиль фото"/>
-              </div>
-              <div className="profile__info">
-                  <h1 id="profileName" className="profile__name">Жак-Ив Кусто</h1>
-                  <button type="button" aria-label="Редактировать" className="button profile__edit-button"></button>
-                  <p id="profileAbout" class="profile__about">Исследователь океана</p>
-              </div>
-              <button type="button" aria-label="Добавить картинку" className="button profile__button-add"></button>
-            </section>
-
-            <section className="elements">
-              <ul className="elements__catalogue"></ul>
-            </section>
-        </div>
-      </main>
+      <Main/>
+      
 
       
         <footer className="footer">
