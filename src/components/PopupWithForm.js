279abc17ad4f3310e2b5f3ae905e7ae64746d08d
tatkuptcova <tatkuptcova@yandex.ru>
2021-07-29 19:27:00 +0200

    реализовано открытие и закрытие popup

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -2,9 +2,9 @@
 function PopupWithForm(props) {
 
     return (
-        <section className={`popup popup_${props.name} ${props.isOpen ? 'popup_is-opened' : ' '}`}>
+        <section className={`popup popup_type_${props.name} ${props.isOpen ? 'popup_opened' : ' '}`}>
             <div className={`popup__content popup__content_${props.name}`}>
-                <button onClick={props.close} type="button" aria-label="Закрыть" className={`button popup__close popup__close_${props.close}`}/>
+                <button onClick={props.onClose} type="button" aria-label="Закрыть" className={`button popup__close popup__close_${props.close}`}/>
                 <h2 className="popup__title">{props.title}</h2>
                 <form name={`form-${props.name}`} className={`popup__form popup__form_${props.name}`} novalidate>
                     {props.children}
