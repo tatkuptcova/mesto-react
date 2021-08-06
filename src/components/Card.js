8249e1bfd96723ffa4c630305694c4a9b490479e
tatkuptcova <tatkuptcova@yandex.ru>
2021-08-06 21:48:16 +0200

    cardDeleteButtonClassName and cardLikeButtonClassName

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,40 @@
+import React from 'react';
 import basket from '../images/basket.svg';
+import { CurrentUserContext } from '../context/CurrentUserContext';
 
 function Card (props) {
+    
+    const currentUser = React.useContext(CurrentUserContext);
 
-    const handleClick = () => {
+    const isOwn = props.card.owner._id === currentUser._id;
+    const cardDeleteButtonClassName = (`button elements__button-delete ${isOwn ? 'button elements__button-delete_visible': ''}`);
+    
+    const isLiked = props.card.likes.some(i => i._id === currentUser._id); 
+    const cardLikeButtonClassName = (`button elements__button-like ${isLiked ? 'button elements__button-like_active' : ''}`);
+    
+
+    function handleClick () {
         props.onCardClick(props.card)
+    }
+
+    function handleLikeClick () {
+        props.onCardLike(props.card)
+    }
+    
+    function handleDeleteClick () {
+        props.onCardDelete(props.card)
     }
 
     return(
         <li className="elements__item">
-            <button type="button" aria-label="Удалить" className="button elements__button-delete">
+            <button type="button" aria-label="Удалить" className={cardDeleteButtonClassName} onClick={handleDeleteClick}>
                 <img src={basket} alt="Удалить"/>
             </button>
             <img className="elements__image" src={props.link} alt="#" onClick={handleClick}/>
             <div className="elements__info">
                 <h2 className="elements__title">{props.name}</h2>
                 <div className="elements__place-like">
-                    <button type="button" aria-label="Нравится" className="button elements__button-like"/>
+                    <button type="button" aria-label="Нравится" className={cardLikeButtonClassName} onClick={handleLikeClick}/>
                     <span className="elements__likes-count">{props.likes}</span>
                 </div>
             </div>
